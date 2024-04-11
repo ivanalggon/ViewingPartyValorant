a72237f8d32c8b9047bb5ffc485834f9dd8ce0f5
María Rodríguez Iranzo <121940607+MariaRodriguezIranzo@users.noreply.github.com>
2024-04-11 09:13:19 +0000

    roting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './components/body/body.component';
+import { NoticiasComponent } from './components/body/noticias/noticias.component';
+import { TiendaComponent } from './components/body/tienda/tienda.component';
+import { ReservasComponent } from './components/body/reservas/reservas.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: BodyComponent }, // Ruta por defecto
+    { path: 'noticias', component: NoticiasComponent },
+    { path: 'tienda', component: TiendaComponent },
+    { path: 'reservas', component: ReservasComponent }
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
