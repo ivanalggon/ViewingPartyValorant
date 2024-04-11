07974b1dfd03e30093a8a7f26afc3a59cc33114b
María Rodríguez Iranzo <121940607+MariaRodriguezIranzo@users.noreply.github.com>
2024-04-11 10:10:10 +0000

    FUNCIONA RUTING

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { BodyComponent } from './components/body/body.component';
-import { NoticiasComponent } from './components/body/noticias/noticias.component';
-import { TiendaComponent } from './components/body/tienda/tienda.component';
-import { ReservasComponent } from './components/body/reservas/reservas.component';
+import { Routes } from '@angular/router';
 
-const routes: Routes = [
-    { path: '', component: BodyComponent }, // Ruta por defecto
-    { path: 'noticias', component: NoticiasComponent },
-    { path: 'tienda', component: TiendaComponent },
-    { path: 'reservas', component: ReservasComponent }
-  ];
-  
-  @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
+export const routes: Routes = [
+    
+];
