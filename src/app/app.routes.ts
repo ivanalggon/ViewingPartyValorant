d8225baa6db025312b9fe529f749a56a55930d4b
María Rodríguez Iranzo <121940607+MariaRodriguezIranzo@users.noreply.github.com>
2024-04-11 10:15:20 +0000

    RUTING enlaces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
+import { BodyComponent } from './components/body/body.component';
+import { NoticiasComponent } from './components/body/noticias/noticias.component';
+import { TiendaComponent } from './components/body/tienda/tienda.component';
+import { ReservasComponent } from './components/body/reservas/reservas.component';
 
 export const routes: Routes = [
-    
+    { path: 'noticias', component: NoticiasComponent },
+    { path: 'tienda', component: TiendaComponent },
+    { path: 'reservas', component: ReservasComponent }
 ];
