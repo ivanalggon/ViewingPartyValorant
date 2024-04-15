68d83fa611198c9aa55b5de541579a02960844bc
Iván Sebastián Alguacil González <ivanalggon@campus.monlau.com>
2024-04-15 18:32:36 +0200

    routing css

--- src/app/pages/home/body.component.ts
+++ src/app/pages/home/body.component.ts
@@ -4,11 +4,11 @@ import { ReservasComponent } from '../reservas/reservas.component';
 import { TiendaComponent } from '../tienda/tienda.component';
 import { NoticiasComponent } from '../noticias/noticias.component';
 import { sliderImgComponent } from '../../components/sliderImg/sliderImg.component';
-
+import { ContactoComponent} from '../contacto/contacto.component';
 
 @Component({
   selector: 'app-body',
-  imports: [CommonModule, ReservasComponent, TiendaComponent, NoticiasComponent,sliderImgComponent],
+  imports: [CommonModule, ReservasComponent, TiendaComponent, NoticiasComponent,sliderImgComponent, ContactoComponent],
   standalone: true,
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
