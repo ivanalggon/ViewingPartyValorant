8c60ba2a24967d2ee47bf69a5275b84dca24313f
Iván Sebastián Alguacil González <ivanalggon@campus.monlau.com>
2024-04-29 20:27:38 +0200

    mejoras letras

--- src/app/pages/tienda/tienda.component.ts
+++ src/app/pages/tienda/tienda.component.ts
@@ -12,37 +12,37 @@ import { Producto } from './producto.interface';
 export class TiendaComponent implements OnInit{
   productos: Producto[] = [
     {
-      nombre: "Figura Jett de Valorant",
+      nombre: "Figura Jett",
       descripcion: "Descripción breve del producto 1",
       precio: 189.99,
       imagen: "../assets/img/jett-figura.jpg"
     },
     {
-      nombre: "Figura Phoenix de Valorant",
+      nombre: "Figura Phoenix",
       descripcion: "Descripción breve del producto 2",
       precio: 189.99,
       imagen: "../assets/img/phoenix-figura.jpg"
     },
     {
-      nombre: "Camiseta de Valorant",
+      nombre: "Camiseta Valorant",
       descripcion: "Descripción breve del producto 3",
       precio: 33.33,
       imagen: "../assets/img/camiseta-valorant.jpg"
     },
     {
-      nombre: "Sudadera de Valorant",
+      nombre: "Sudadera Valorant",
       descripcion: "Descripción breve del producto 4",
       precio: 15.99,
       imagen: "../assets/img/sudadera-valorant.jpg"
     },
     {
-      nombre: "Chaqueta Cazadora de Valorant",
+      nombre: "Chaqueta Cazadora",
       descripcion: "Descripción breve del producto 5",
       precio: 24.99,
       imagen: "../assets/img/chaqueta-valorant.jpg"
     },
     {
-      nombre: "Jersey de Valorant",
+      nombre: "Jersey Valorant",
       descripcion: "Descripción breve del producto 6",
       precio: 9.99,
       imagen: "../assets/img/jersey-valorant.jpg"
