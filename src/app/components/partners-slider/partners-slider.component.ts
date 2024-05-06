425c86f9e3608bb9e40385742a2fa145174d7a1b
Iván Sebastián Alguacil González <ivanalggon@campus.monlau.com>
2024-05-06 16:39:11 +0200

    partners-slider component - Contacto

---
+++ src/app/components/partners-slider/partners-slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partners-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './partners-slider.component.html',
+  styleUrl: './partners-slider.component.css'
+})
+export class PartnersSliderComponent {
+  
+}
--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PartnersSliderComponent } from '../../components/partners-slider/partners-slider.component';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [PartnersSliderComponent],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
